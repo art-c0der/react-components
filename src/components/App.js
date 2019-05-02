bb376f0a5ac2b3cd055a5bf9b7ac584ea72a4cc5
Temu4 <artem.vorobjov@gmail.com>
2019-05-03 00:35:03 +0300

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+
+import './App.css';
+
+class App extends Component {
+  toggleSidebar = e => {
+    const sidebar = document.querySelector('.sidebar');
+    const sidebarButton = document.querySelector('.hamburger');
+    sidebar.classList.toggle('active');
+    sidebarButton.classList.toggle('active');
+    e.stopPropagation();
+  };
+
+  hideSidebar = () => {
+    const sidebar = document.querySelector('.sidebar');
+    const sidebarButton = document.querySelector('.hamburger');
+    sidebar.classList.remove('active');
+    sidebarButton.classList.remove('active');
+  };
+
+  render() {
+    return (
+      <>
+        <header className="header">
+          <button className="hamburger" onClick={this.toggleSidebar}>
+            <div className="hamburger__box">
+              <div className="hamburger__line" />
+            </div>
+          </button>
+          <h2 className="header__title">React Components Library</h2>
+          <div className="header__buttons">
+            <button className="button button--white">Sing In</button>
+            <button className="button button--white">Sing Up</button>
+            <button className="button button--red">Log Out</button>
+          </div>
+        </header>
+        <nav className="sidebar">
+          <ul>
+            <li className="nav__link">1</li>
+            <li className="nav__link">2</li>
+            <li className="nav__link">3</li>
+            <li className="nav__link">4</li>
+            <li className="nav__link">5</li>
+          </ul>
+        </nav>
+        <main className="main" onClick={this.hideSidebar} />
+      </>
+    );
+  }
+}
+
+export default App;
